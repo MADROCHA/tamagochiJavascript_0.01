3e2ba1cb2c42e1ac52e387fc2af2900d4b06a6d6
MADROCHA <madlarocha@gmail.com>
2022-08-08 21:11:55 +0200

    methods.Tamagochi_play.eat.sleep\tamagochi.js\Tamagochi_1.0_MAD

--- tamagochi.js
+++ tamagochi.js
@@ -1,110 +1,50 @@
 class Tamagochi {
-    constructor( hunger, mood, name, stamina) {
+    constructor( hunger = 4, mood = 4, name, stamina = 4) {
         this._hunger = hunger;
         this._mood = mood;
         this._name = name;
         this._stamina = stamina;
-        
-    }
-    get hunger (){
-        return this._hunger;
-    }
-    get mood (){
-        return this._mood;
-    }
-    get stamina (){
-        return this._stamina;
-    }
-    eat (hunger) {
-        this.Tamagochi.hunger = this.Tamagagochi.hunger - 2,
-        this.Tamagochi.this.stamina = this.Tamagagochi.stamina - 1;
-        
-    }
-    play() {
-        return  this.hunger = this.hunger + 1,
-                this.mood = this.mood + 1,
-                this.stamina = this.stamina -1;
-    }
-    sleep(_) {
-        return this.stamina = this.stamina +2;
-    }
-        
-        
 
+        
+        console.log(Tamagochi)
+    
+    }
+
+    tamagochiEat(_stamina, _hunger){
+    }
+        
+    tamagochiPlay( _hunger, _mood, _stamina){
+    }
+
+    tamagochiSleep(_stamina){
+    }
 }
 
 
 
 
-const axolotlTamagochi = new Tamagochi( 4, 4 , 'Axolotl', 4)
-/* let axolotlTamagochi = new Tamagochi( 4, 4, 'axolotl', 4) */
 
 
-
-console.log(Tamagochi)
+const axolotlTamagochi = new Tamagochi( 4, Tamagochi._mood,'Axolotl', 5)
 console.log(axolotlTamagochi)
 
-/* console.log(Tamagochi.eat)
-console.log(Tamagochi.sleep)
-console.log(Tamagochi.hunger) */
+console.log(axolotlTamagochi.tamagochiEat)
 
-console.log(axolotlTamagochi.eat)
+console.log(axolotlTamagochi.tamagochiPlay)
+console.log(axolotlTamagochi.tamagochiSleep)
+
+console.log(Tamagochi.tamagochiEat)
+
+
+/* console.log(axolotlTamagochi.eat)
 console.log(axolotlTamagochi.play)
-console.log(axolotlTamagochi.sleep)
+console.log(axolotlTamagochi.sleep) */
 
-console.log(axolotlTamagochi.hunger)
-console.log(axolotlTamagochi.mood)
-console.log(axolotlTamagochi.stamina)
-
-
-/* class Tamagochi {
-    constructor( hunger, mood, name, stamina) {
-        this.hunger = hunger;
-        this.mood = mood;
-        this.name = name;
-        this.stamina = stamina;
-        
-    }
-    eat (stamina) {
-        return stamina = x - 1;
-        
-    }
-    play(mood, stamina, hunger) {
-        return  mood = this.mood + 1,
-                this.stamina = stamina -1,
-                hunger +1;
-    }
-    sleep(stamina) {
-        return this.stamina = stamina
-    }
-}
-
-let hunger = 4;
-let mood = 4;
-let stamina = 4;
-
-
-let axolotlTamagochi = new Tamagochi( hunger, mood , 'Axolotl', stamina)
-/* let axolotlTamagochi = new Tamagochi( 4, 4, 'axolotl', 4) */
-
-/* Tamagochi */ 
+console.log(axolotlTamagochi._hunger)
+console.log(axolotlTamagochi._mood)
+console.log(axolotlTamagochi._stamina)
+console.log(axolotlTamagochi._name)
 
 
 
-/* --- */
-
-/* class Tamagochi {
-    constructor(eat, play, sleep) {
-        this.eat = eat;
-        this.play = play;
-        this.sleep = sleep;
-        
-    }
-}
-
-let axolotlTamagochi = new Tamagochi(4, 4, 4)
-
-Tamagochi
-
-console.log(axolotlTamagochi) */
 
