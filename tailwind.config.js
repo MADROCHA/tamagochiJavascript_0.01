c9e40a17f9380ebd3c16ee2298935c7e01ec20d7
MADROCHA <madlarocha@gmail.com>
2022-08-10 01:51:36 +0200

    tailwind_install_DONE\tamagochiYawn_getById_styleIssues\\Tamagochi_1.1_MAD

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */ 
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      
+    colors: {
+      'colorDarkPurple': '#371f2f',
+      'colorDarkGreen': '#235a5c',
+      'colorMidPink': '#d88f8f',
+      'colorPaleWhite': '#ced7e8',
+
+    },
+  },
+  plugins: [],
+  },
+}
