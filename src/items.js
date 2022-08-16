1c07a808d1f400e055decd3c4bf0e0afa39162a5
MADROCHA <madlarocha@gmail.com>
2022-08-16 13:01:42 +0200

    items.js_function_countBits_workingOn_replyMoneyAmount\tamagochiYawn_getById_IssuesTiming\\Tamagochi_1.3_MAD

--- src/items.js
+++ src/items.js
@@ -27,16 +27,16 @@ var worldItems = [
     waterCarrot = new Food ("Water Carrot", 0, ),
     soupFlowers = new Food ("Soup of Flowers", 0, ),
     axolotlGold = new Item ("Axolotl Reward", 0),
-    bitBag = new Currency (50, 1, "Bit Poach", 2 ) ,
+    bitBag = new Currency (0, , "Bit Poach", 2 ) 
 ];
-waterCarrot.effect = "foodEffect, increases stamina by 1 (next day?) && decreases hunger by 1 (instant?) "
-waterCarrot.description = "somewhat it seems yummy"
+waterCarrot.effect = "foodEffect, increases stamina by 1 (next day?) && decreases hunger by 1 (instant?) ";
+waterCarrot.description = "somewhat it seems yummy";
 
-soupFlowers.efect = "foodEffect, increases stamina & mood by 1 - decreases hunger by 1"
-soupFlowers.description = "food.. but doesn't fit your taste"
+soupFlowers.efect = "foodEffect, increases stamina & mood by 1 - decreases hunger by 1";
+soupFlowers.description = "food.. but doesn't fit your taste";
 
-axolotlGold._description = "friendship is something beautiful, isn't really? "
-bitBag._description = "the fabric is a bit worn, yet useful"
+axolotlGold._description = "friendship is something beautiful, isn't really? ";
+bitBag._description = "the fabric is a bit worn, yet useful";
 
 var inventory = [];
 for (let i of worldItems) {
@@ -45,37 +45,42 @@ for (let i of worldItems) {
     }
 };
 
-function countBits(bitBag) {
+
+function countBits(worldItems) {
     let countBitsReply = `Your open ${bitBag._name} holds `;
     if (bitBag._bit) {
-        countBitsReply = countBitsReply + `${bitBag._bit}x bit`;
+        countBitsReply = countBitsReply + `${bitBag._bit} bit `;
         if (bitBag._goldChunck && bitBag._bit) {
-                countBitsReply = countBitsReply + " and";
+                countBitsReply = countBitsReply + "and ";
         };
     };
     if (bitBag._goldChunck) {
-        countBitsReply = countBitsReply + `${bitBag._goldChunck}x guile`;
-        if (bitBag._goldChunck && bitBag._bit) {
-            countBitsReply = countBitsReply + `${bitBag._goldChunck}x guile`;
-        };
+        countBitsReply = countBitsReply + `${bitBag._goldChunck}x guile `;
+
     };
     if (!bitBag._goldChunck && !bitBag._bit) {
-        countBitsReply = countBitsReply + "no";
+        countBitsReply = countBitsReply + "no ";
     };
-    countBitsReply = countBitsReply + " chunck"
-    if (bitBag._bit > 1 || (bitBag._bit === 0 && bitBag._goldChunck > 1) || (!bitBag._goldChunck && !bitBag._bit)) {
+    countBitsReply = countBitsReply + "chunck"
+    if (bitBag._bit > 1 || (bitBag._bit > 0 && bitBag._goldChunck > 0) || (!bitBag._goldChunck && !bitBag._bit)) {
         countBitsReply = countBitsReply + "s"
     };
-    countBitsReply = countBitsReply + ".";
-    console.log(countBitsReply)
+    countBitsReply = countBitsReply + "."
+    console.log(countBitsReply);
+    console.log(bitBag);
+    
 };
+countBits();
 
 
+console.log(worldItems[3]);
 console.log(inventory);
 console.log(bitBag);
 console.log(bitBag._name);
 console.log(bitBag._bit);
+
 console.log(countBits());
+
 
 
 
